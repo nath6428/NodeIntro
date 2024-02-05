d137dba2245af6502bbf6ea91323e0a5a2eb9943
Nathan Crasto <nathancrasto6@gmail.com>
2024-02-05 17:00:59 +0400

    Basic routing

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 
 const httpServer = http.createServer((req, res) => {
     
-    res.writeHead(200)
-    res.write(`Hey! Connection made!, ${req.url}`)
+    res.writeHead(200, {"Content-Type": "text/html"})
+
+    switch(req.url){
+
+        case("/"):
+            res.write(fs.readFileSync(`./index.html`));
+            break
+        case("/about"):
+            res.write(fs.readFileSync(`./about.html`));
+            break
+        case("/contact-me"):
+            res.write(fs.readFileSync(`./contact-me.html`));
+            break
+        default:
+            res.write(fs.readFileSync(`./404.html`));
+            break
+        
+    }
     res.end()
 
 }).listen(7128)
